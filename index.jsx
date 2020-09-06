b3c3f9cbca258a891ad1d7836998d64e108ae7c4
Alex Sun <alexsun82@icloud.com>
2020-09-06 11:39:50 +0200

    Apply new style names

--- index.jsx
+++ index.jsx
@@ -2,24 +2,33 @@ import { run } from "uebersicht";
 import {
   container,
   flexSeparator,
-  workspaceStyle,
-  processStyle,
-  netstatStyle,
-  batteryStyle,
-  datetimeStyle,
+  bgNone,
+  bgPurple,
+  bgDarkPink,
+  bgPink,
+  bgBrightPink,
 } from "./lib/styles";
 
-export const refreshFrequency = 5000;
+export const refreshFrequency = 10000;
 
 export const command = async (dispatch) => {
   const pWorkspace = run("marsbar/scripts/workspace.sh");
   const pProcess = run("marsbar/scripts/process.sh");
+  const pMail = run("marsbar/scripts/mail.scpt");
   const pNetstat = run("marsbar/scripts/netstat.sh");
   const pBattery = run("marsbar/scripts/battery.sh");
   const pDatetime = run("marsbar/scripts/datetime.sh");
-  const [workspace, process, netstat, battery, datetime] = await Promise.all([
+  const [
+    workspace,
+    process,
+    mail,
+    netstat,
+    battery,
+    datetime,
+  ] = await Promise.all([
     pWorkspace,
     pProcess,
+    pMail,
     pNetstat,
     pBattery,
     pDatetime,
@@ -27,7 +36,7 @@ export const command = async (dispatch) => {
 
   dispatch({
     type: "UPDATE",
-    payload: { workspace, process, netstat, battery, datetime },
+    payload: { workspace, process, mail, netstat, battery, datetime },
   });
 };
 
@@ -41,13 +50,21 @@ export const updateState = (action, state) => {
   }
 };
 
-export const render = ({ workspace, process, netstat, battery, datetime }) => (
+export const render = ({
+  workspace,
+  process,
+  mail,
+  netstat,
+  battery,
+  datetime,
+}) => (
   <div className={container}>
-    <div className={workspaceStyle}>{workspace}</div>
-    <div className={processStyle}>{process}</div>
+    <div className={bgPurple}>{workspace}</div>
+    <div className={bgNone}>{process}</div>
     <div className={flexSeparator} />
-    <div className={netstatStyle}>{netstat}</div>
-    <div className={batteryStyle}>{battery}</div>
-    <div className={datetimeStyle}>{datetime}</div>
+    <div className={bgNone}>{mail > 0 ? `\uf0e0 \u00a0${mail}` : ""}</div>
+    <div className={bgDarkPink}>{netstat}</div>
+    <div className={bgPink}>{battery}</div>
+    <div className={bgBrightPink}>{datetime}</div>
   </div>
 );
